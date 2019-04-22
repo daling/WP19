bd6b1a218b8a49fa72a4ba27ad94960c667627d1
Rolf <rolfdaling@gmail.com>
2019-04-22 16:52:43 +0200

    Added js file to the html pages and made an if/else to change the doc title.

---
+++ assignment_1/js/main.js
@@ -0,0 +1,6 @@
+// Changes the document title depending on which page you are
+if (window.location.pathname === '/WP19/assignment_1/index.html') {
+    document.title = 'Webprogramming (LIX019P05) - Index';
+} else if (window.location.pathname === '/WP19/assignment_1/second.html') {
+    document.title = 'Webprogramming (LIX019P05) - Second';
+}
