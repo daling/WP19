425b599cb198d47ba62f3c0b8624d9a1d71500f9
Rolf <rolfdaling@gmail.com>
2019-04-23 17:34:44 +0200

    Article adding updated, now adds at correct page.

--- assignment_1/js/main.js
+++ assignment_1/js/main.js
@@ -1,7 +1,10 @@
+var index = '/WP19/assignment_1/index.html';
+var second = '/WP19/assignment_1/second.html';
+
 // Changes the document title depending on which page you are
-if (window.location.pathname === '/WP19/assignment_1/index.html') {
+if (window.location.pathname === index) {
     document.title = 'Webprogramming (LIX019P05) - Index';
-} else if (window.location.pathname === '/WP19/assignment_1/second.html') {
+} else if (window.location.pathname === second) {
     document.title = 'Webprogramming (LIX019P05) - Second';
 }
 
@@ -10,9 +13,10 @@ if (window.location.pathname === '/WP19/assignment_1/index.html') {
  * @param element The element in which to insert the article.
  * @param header String with the header text.
  * @param text String with the text for the article.
+ * @param pagePath String with the pathname of the page it needs to insert to.
  * ToDo: multiple paragraphs option?
  */
-function addArticle(element, header, text) {
+function addArticle(element, header, text, pagePath) {
     var newArticle = document.createElement('article');
 
     var newHeader = document.createElement('h1');
@@ -30,12 +34,14 @@ function addArticle(element, header, text) {
     newArticle.appendChild(newHeader);
     newArticle.appendChild(newParagraph);
 
-    var pos = element[0];
+    if (window.location.pathname === pagePath) {
+        var pos = element[0];
 
-    pos.appendChild(newArticle);
+        pos.appendChild(newArticle);
+    }
 }
 
 var mainCol = document.getElementsByClassName('col-md-12');
 var newArticleHeader = 'This is my second article';
 var newArticleText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget eros ultrices, dapibus lacus ultrices, ultrices tortor. Nam tincidunt blandit neque, at ornare sapien ultricies ut. Integer eget ultricies velit. Cras eu tellus ex. Integer bibendum nisi neque, sed auctor odio blandit sit amet. Aenean augue tellus, tincidunt vel commodo bibendum, rutrum nec augue. Donec pulvinar sem in purus congue sodales. Nam magna urna, maximus ut eros vel, rutrum semper sem. Duis a efficitur mauris. Nunc a aliquam nisi, vel iaculis justo. Donec lacus nulla, sollicitudin vitae lectus vel, tempus vestibulum ipsum. In dignissim consequat pellentesque. Pellentesque eget eleifend velit. Aenean aliquam auctor nibh vitae tristique. Nulla facilisi.';
-addArticle(mainCol, newArticleHeader, newArticleText);
+addArticle(mainCol, newArticleHeader, newArticleText, index);
