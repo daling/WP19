d98e534dd395ffd9dbf34fc666359271516150e9
Rolf <rolfdaling@gmail.com>
2019-04-25 23:35:09 +0200

    Moved main function to the bottom.

--- assignment_1/js/main.js
+++ assignment_1/js/main.js
@@ -1,32 +1,3 @@
-window.addEventListener('DOMContentLoaded', main, false);
-
-function main(){
-    // 1
-    changeTitle('index.html', 'Webprogramming (LIX019P05) - Index');
-    changeTitle('second.html', 'Webprogramming (LIX019P05) - Second');
-
-    // 2
-    const mainCol = document.getElementsByClassName('col-md-12');
-    const newArticleHeader = 'This is my second article';
-    const newArticleText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget eros ultrices, dapibus lacus ultrices, ultrices tortor. Nam tincidunt blandit neque, at ornare sapien ultricies ut. Integer eget ultricies velit. Cras eu tellus ex. Integer bibendum nisi neque, sed auctor odio blandit sit amet. Aenean augue tellus, tincidunt vel commodo bibendum, rutrum nec augue. Donec pulvinar sem in purus congue sodales. Nam magna urna, maximus ut eros vel, rutrum semper sem. Duis a efficitur mauris. Nunc a aliquam nisi, vel iaculis justo. Donec lacus nulla, sollicitudin vitae lectus vel, tempus vestibulum ipsum. In dignissim consequat pellentesque. Pellentesque eget eleifend velit. Aenean aliquam auctor nibh vitae tristique. Nulla facilisi.';
-    addArticle(mainCol, newArticleHeader, newArticleText, 'index.html');
-
-    // 3
-    const link = changeLink();
-
-    // 4
-    linkBlank(link);
-
-    // 5
-    redMaker('nav-link');
-
-    // 6
-    // insertTable();
-
-    // 7
-    addSidebar();
-}
-
 /**
  * Changes the title of the page.
  * @param page String of the filename of the page.
@@ -101,7 +72,6 @@ function linkBlank(link) {
     link.setAttribute('target', '_blank');
 }
 
-
 /**
  * Changes the color of elements it retrieves by the class name.
  * @param className String with the name of the class you want to color things red from.
@@ -112,8 +82,6 @@ function redMaker(className) {
         navItems[i].style.color = 'red';
     }
 }
-
-
 
 // 6
 // function insertTable() {
@@ -153,3 +121,35 @@ function addSidebar() {
     }
 }
 
+/**
+ * This function is called when all the DOM content is loaded and call each function
+ * that is used for every specific task.
+ */
+function main(){
+    // 1
+    changeTitle('index.html', 'Webprogramming (LIX019P05) - Index');
+    changeTitle('second.html', 'Webprogramming (LIX019P05) - Second');
+
+    // 2
+    const mainCol = document.getElementsByClassName('col-md-12');
+    const newArticleHeader = 'This is my second article';
+    const newArticleText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget eros ultrices, dapibus lacus ultrices, ultrices tortor. Nam tincidunt blandit neque, at ornare sapien ultricies ut. Integer eget ultricies velit. Cras eu tellus ex. Integer bibendum nisi neque, sed auctor odio blandit sit amet. Aenean augue tellus, tincidunt vel commodo bibendum, rutrum nec augue. Donec pulvinar sem in purus congue sodales. Nam magna urna, maximus ut eros vel, rutrum semper sem. Duis a efficitur mauris. Nunc a aliquam nisi, vel iaculis justo. Donec lacus nulla, sollicitudin vitae lectus vel, tempus vestibulum ipsum. In dignissim consequat pellentesque. Pellentesque eget eleifend velit. Aenean aliquam auctor nibh vitae tristique. Nulla facilisi.';
+    addArticle(mainCol, newArticleHeader, newArticleText, 'index.html');
+
+    // 3
+    const link = changeLink();
+
+    // 4
+    linkBlank(link);
+
+    // 5
+    redMaker('nav-link');
+
+    // 6
+    // insertTable();
+
+    // 7
+    addSidebar();
+}
+
+window.addEventListener('DOMContentLoaded', main, false);
