87d8c9b0ebe1d55e5ab4d5d1439d99eb546e71e6
Rolf <rolfdaling@gmail.com>
2019-05-07 18:10:41 +0200

    Made the tabs work.

--- assignment_2/js/main.js
+++ assignment_2/js/main.js
@@ -1,11 +1,32 @@
 function validateForm(){
-
+    // $("#inputName").
 }
 
 function writeFormData(){
 
 }
 
+/**
+ * Document ready function
+ */
 $(function() {
+    $("#submit").click(function () {
+        validateForm();
+    });
+    $("#erase").click(function () {
+        $("#form")[0].reset();
+    });
+    $("#link-tab").click(function () {
+        $("div.tab-pane").toggleClass("active");
+        $("div.links").addClass("active");
+        $("#link-tab").addClass("active");
+        $("#contact-tab").removeClass("active");
 
+    });
+    $("#contact-tab").click(function () {
+        $("div.tab-pane").toggleClass("active");
+        $("div.contact").addClass("active");
+        $("#link-tab").removeClass("active");
+        $("#contact-tab").addClass("active");
+    });
 });
