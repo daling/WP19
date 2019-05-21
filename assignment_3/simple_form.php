2d00f5e32dec5d822d577f6b9295d067d8ed324e
Rolf <rolfdaling@gmail.com>
2019-05-21 19:30:41 +0200

    Restructured and added form to the page.

--- assignment_3/simple_form.php
+++ assignment_3/simple_form.php
@@ -13,7 +13,21 @@ $navigation = Array(
 include __DIR__ . '/tpl/head.php';
 include __DIR__ . '/tpl/body_start.php';
 ?>
-
+<div class="row wp-row">
+    <div class="col-md-12">
+        <form method="POST">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Enter name">
+            </div>
+            <div class="form-group">
+                <label for="text">Place of residence</label>
+                <input type="text" class="form-control" id="place" name="place" placeholder="Enter place of residence">
+            </div>
+            <button class="btn btn-success mb-2" id="submit-btn">Submit</button> <button class="btn btn-danger mb-2" id="erase-btn">Erase</button>
+        </form>
+    </div>
+</div>
 <?php
 include __DIR__ . '/tpl/body_end.php';
 ?>
